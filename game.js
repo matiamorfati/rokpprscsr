c7582a18717fd330ed19292eb2bee2bbc9948aaf
matiamorfati <121320678+matiamorfati@users.noreply.github.com>
2023-10-30 14:35:09 +0100

    Pretty much killed it. Finish the project.

--- game.js
+++ game.js
@@ -1,7 +1,158 @@
 
+let userScore = 0;
+let computerScore = 0;
+
+
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const rock_button = document.getElementById("rock");
+const paper_button = document.getElementById("paper");
+const scissors_button = document.getElementById("scissors");
+const computerChoice_div = document.getElementById("enemy");
+const outcome_div = document.getElementById("winner");
+
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+
+function showComputerChoice(computerChoice) {
+    switch(computerChoice) {
+        case 'r':
+            computerChoice_div.innerHTML = "rock";
+            break;
+        case 'p':
+            computerChoice_div.innerHTML = "paper";
+            break;       
+        case 's':
+            computerChoice_div.innerHTML = "scissors";
+            break;
+    }
+}
+
+function outcomeConversion(choice) {
+    if(choice === "r") return "Rock";
+    if(choice === "p") return "Paper";
+    if(choice === "s") return "Scissors";
+}
+
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    outcome_div.innerHTML = outcomeConversion(userChoice) + " beats " + outcomeConversion(computerChoice) + ". You win!";
+}
+
+
+function fail(userChoice, computerChoice) {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    outcome_div.innerHTML = outcomeConversion(computerChoice) + " beats " + outcomeConversion(userChoice) + ". You fail."
+}
+
+
+function tie(userChoice, computerChoice) {
+outcome_div.innerHTML = outcomeConversion(userChoice) + " with the " + outcomeConversion(computerChoice) + "? It can only be a tie!"
+}
+
+
+function game(userChoice) {
+    
+
+    const computerChoice = getComputerChoice();
+    showComputerChoice(computerChoice);
+
+
+    switch(userChoice + computerChoice){
+
+        
+        case "pr":
+        case "sp":
+        case "rs":
+            win(userChoice, computerChoice);
+            break;
+
+        case "rp":
+        case "ps":
+        case "sr":
+            fail(userChoice, computerChoice);
+            break;
+
+        case "rr":
+        case "pp":
+        case "ss":
+            tie(userChoice, computerChoice);
+            break;
+
+    }
+
+}
+
+function main() {
+   
+rock_button.addEventListener('click', function() {
+    game("r");
+})
+
+paper_button.addEventListener('click', function() {
+    game("p");
+})
+
+scissors_button.addEventListener('click', function() {
+    game("s");
+})
+
+}
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 function getRandomNum(max) {
     return Math.floor(Math.random() * max);
 }
+
 function computerChoice() {
     let n = getRandomNum(3)
 
@@ -55,3 +206,4 @@ for(let i = 0; i < 5; i++){
     let computer = computerChoice()
     game(player, computer)
 }
+*/
